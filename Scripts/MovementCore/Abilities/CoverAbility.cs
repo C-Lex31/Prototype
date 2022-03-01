67d42b68ac236ae9097fb3ae0e5cad7fdae292ed
CppLexicon <90330805+CppLexicon@users.noreply.github.com>
2022-03-01 22:50:32 +0530

    Update CoverAbility.cs

--- Scripts/MovementCore/Abilities/CoverAbility.cs
+++ Scripts/MovementCore/Abilities/CoverAbility.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum CoverMovementInputType { Relative, Absolute }
+public enum CoverType { Stand, Crouch, MidCrouch }
 public class CoverAbility : ThirdPersonAbility
 {
     public LayerMask CoverMask;
     public Transform CastPoint_1;
+    public Transform CastPoint_1H;
+     public Transform CastPoint_2H;
     public Transform CastPoint_2;
     public Transform CastPoint_3;
     public Transform CastPoint_4;
-    RaycastHit CoverHitInfo;
+
     [SerializeField] private CoverMovementInputType m_MovementInput = CoverMovementInputType.Relative;
+    //------------------------------INTERNAL VARS AND COMPONENTS--------------------------
+    RaycastHit CoverHitInfo;
+    CoverType type;
+
+ 
+    //-------------------------------------------------------------------------------------
     private Vector3 InputMove
     {
-
 
         get
         {
@@ -25,11 +33,15 @@ public class CoverAbility : ThirdPersonAbility
     }
     public override bool TryEnterAbility()
     {
-        // Physics.Raycast()
-        // Physics.Raycast(CastPoint_3.localPosition , transform.forward, 1.5f,CoverMask) && Physics.Raycast(CastPoint_4.localPosition, transform.forward, 1.5f,CoverMask);
-        if (Physics.Raycast(CastPoint_3.position, transform.forward, out CoverHitInfo, 1f, CoverMask) && Physics.Raycast(CastPoint_4.position, transform.forward, out CoverHitInfo, 1f, CoverMask))
+        if (Physics.Raycast(CastPoint_3.position, transform.forward, out CoverHitInfo, 0.65f, CoverMask) && Physics.Raycast(CastPoint_4.position, transform.forward, out CoverHitInfo, 0.65f, CoverMask))
         {
-
+            type = CoverType.Stand;
+            return true;
+        }
+        else if (Physics.Raycast(CastPoint_1.position, transform.forward, out CoverHitInfo, 0.65f, CoverMask) && Physics.Raycast(CastPoint_2.position, transform.forward, out CoverHitInfo, 0.65f, CoverMask))
+        {
+            type = CoverType.Crouch;
+           
             return true;
         }
         else
@@ -39,33 +51,79 @@ public class CoverAbility : ThirdPersonAbility
     public override void OnEnterAbility()
     {
         base.OnEnterAbility();
-        m_AnimatorManager.PerformBoolEvent("IsInCover", true);
-        transform.rotation = Quaternion.Lerp(transform.rotation,
+        if (type is CoverType.Stand)
+        {
+            m_AnimatorManager.PerformBoolEvent("IsInCover", true);
+            transform.rotation = Quaternion.Lerp(transform.rotation,
           Quaternion.LookRotation(CoverHitInfo.normal), 20f);
-
+        }
+        if (type is CoverType.Crouch)
+        {
+            m_AnimatorManager.PerformBoolEvent("IsInCrouchCover", true);
+            
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                                   Quaternion.LookRotation(Vector3.Cross(CoverHitInfo.normal, Vector3.up)), 20f *Time.deltaTime );
+        }
     }
     public override void FixedUpdateAbility()
     {
         base.FixedUpdateAbility();
+        if (type is CoverType.Stand)
+            StandCover();
+        if (type is CoverType.Crouch)
+            CrouchCover();
+    }
+    void StandCover()
+    {
         transform.rotation = Quaternion.Lerp(transform.rotation,
-         Quaternion.LookRotation(CoverHitInfo.normal), 20f);
-        if (InputMove.x < -0.1f)
+               Quaternion.LookRotation(CoverHitInfo.normal), 20f);
+
+
+        //-------------------------------MOVEMENT INPUTS---------------------------------------------------------
+        if (InputMove.x < -0.1f && (Physics.Raycast(CastPoint_3.position, -transform.forward, 1.0f, CoverMask)))
             m_AnimatorManager.SetFloatParameter("Horizontal", -1);
-        //  else
-        //    m_AnimatorManager.SetFloatParameter("Horizontal", 0);
-        else if (InputMove.x > 0.1f)
+
+        else if (InputMove.x > 0.1f && (Physics.Raycast(CastPoint_4.position, -transform.forward, 1.0f, CoverMask)))
             m_AnimatorManager.SetFloatParameter("Horizontal", 1);
         else
+        {
             m_AnimatorManager.SetFloatParameter("Horizontal", 0);
 
-        if(!(Physics.Raycast(CastPoint_4.position,-transform.forward,1.0f,CoverMask)))
+            if (InputMove.x > 0.1f)
+            {
+                m_AnimatorManager.SetFloatParameter("Peek", 1);
+
+            }
+            else if (InputMove.x < -0.1f)
+            {
+                m_AnimatorManager.SetFloatParameter("Peek", -1);
+            }
+            else
+                m_AnimatorManager.SetFloatParameter("Peek", 0);
+
+        }
+        //-------------------------------------------------------------------------------------------------------
+        Debug.DrawRay(CastPoint_3.position, transform.forward, Color.yellow);
+        Debug.DrawRay(CastPoint_4.position, transform.forward, Color.green);
+    }
+
+    void CrouchCover()
+    {
+        transform.rotation = Quaternion.Lerp( transform.rotation,Quaternion.LookRotation(Vector3.Cross(CoverHitInfo.normal, Vector3.up)),
+                       20f * Time.deltaTime);
+      
+       
+        if (InputMove.y < -0.1f && (Physics.Raycast(CastPoint_1H.position,Vector3.Cross(transform.forward,Vector3.up)  , 2.0f, CoverMask)))
+            m_AnimatorManager.SetFloatParameter("Horizontal", -1);
+
+        else if (InputMove.y > 0.1f && (Physics.Raycast(CastPoint_2H.position, Vector3.Cross(transform.forward,Vector3.up) , 2.0f, CoverMask)))
+            m_AnimatorManager.SetFloatParameter("Horizontal", 1);
+        else
         {
             m_AnimatorManager.SetFloatParameter("Horizontal", 0);
         }
-
-
-Debug.DrawRay(CastPoint_3.position, transform.forward,Color.yellow);
-Debug.DrawRay(CastPoint_4.position, transform.forward,Color.green);
-
+         Debug.DrawRay(CastPoint_1H.position,Vector3.Cross(transform.forward,Vector3.up) , Color.yellow);
+        Debug.DrawRay(CastPoint_2H.position,Vector3.Cross(transform.forward,Vector3.up) , Color.green);
     }
+
 }
